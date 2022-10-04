d1a499df9c949795422f3947866597b381b3ea83
said <Saidrivas527@gmail.com>
2022-10-04 10:50:17 -0600

    Se modifico el archivo Constante.cpp y se agrego el archivo Pitagoras.cpp

---
+++ Pitagora.cpp
@@ -0,0 +1,21 @@
+/*
+Nombre del archivo: pitagoras.cpp
+Autor: Said rivas
+Lugar: ITV
+Instrucciones: Ejemplo sobre la biblioteca cmath
+*/
+#include<iostream>
+#include<cmath>
+using namespacestd;
+int main(){
+	double cateto1, cateto2, hipotenusa;
+	cout << "Ingrese el cateto 1: ";
+	cin >> cateto1;
+	cout << "Ingrese el cateto 2: ";
+	cin >> cateto2;
+	hipotenusa = sqrt ( pow(cateto1,2)+ pow(cateto2,2));
+	cout << "Cateto 1 = " << cateto1 << end;
+	cout << "Cateto 2 = " << Cateto2 << end;
+	cout << "Hipotenusa = " << Hipotenusa << end;
+	retur 0;
+}
