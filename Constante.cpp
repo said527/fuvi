a7a81120a170e1c603c22f6f6619e126c4776219
said <Saidrivas527@gmail.com>
2022-10-04 10:15:34 -0600

    Se agrego un nuevo archivo

--- Constante.cpp
+++ Constante.cpp
@@ -4,9 +4,17 @@ autor: Danilo Nogurea
 lugar: ITV
 Instrucciones: Esto solo es una plantilla
 */
-#incluide<iostream>
+#include<iostream>
 using namespace std;
+//variables globales
+  const double pi = 3.141692;
 int main(){
-
- return0;
+	
+   double radio, area;
+   cout <<"Ingrese el radio del circulo: ";
+   cin >>radio;
+   area = pi * radio;
+   cout << "El area de un circulo con un radio " << radio << " es " <<area;
+    
+ return 0;
 }
