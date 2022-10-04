c0a1010d16122871cc1fcffefd62895a94e58d5b
said <Saidrivas527@gmail.com>
2022-10-04 09:40:17 -0600

    Se agrego de nuevo el archivo Constante.cpp

---
+++ Constante.cpp
@@ -0,0 +1,12 @@
+/*
+nombre del archivo: sumanumeros.cpp
+autor: Danilo Nogurea
+lugar: ITV
+Instrucciones: Esto solo es una plantilla
+*/
+#incluide<iostream>
+using namespace std;
+int main(){
+
+ return0;
+}
