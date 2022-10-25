e47d6b83c90dfd026aa3b72976c1c3b1cf074d66
said527 <Saidrivas527@gmail.com>
2022-10-25 09:47:29 -0600

    Este es el ejemplo del test de covid

---
+++ TestCovid.cpp
@@ -0,0 +1,24 @@
+/*
+Nombre del archivo: tienecovid.cpp
+Autor: Natalia Sequeira 
+Lugar: ITV
+Instrucciones: Ejemplos con estructura condicional
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	//Deficion variables
+	int Nombre, Edad;
+	int Fiebre, Tos, Cansancio, Pérdida del gusto u olfato,
+	
+	//Datos entrada 
+	cout <<"Ingrese su nombre: ";
+	cin >>Nombre;
+	cout <<"Ingrese su edad: ";
+	cin >>Edad;
+	
+	//Proceso
+	cout <<"Usted presenta fiebre: ("
