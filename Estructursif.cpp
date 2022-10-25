6c0ddaf362af7e052401c6ad7383462f6076263b
said527 <Saidrivas527@gmail.com>
2022-10-25 11:03:34 -0600

    Ejercicio de Estructursif.cpp

---
+++ Estructursif.cpp
@@ -0,0 +1,27 @@
+/*
+Nombre del archivo: estructuraif.cpp
+Autor: Natalia Sequeira 
+Lugar: ITV
+Instrucciones: Ejemplos con estructura condicional
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	//Estructura condicional o selectiva if
+    
+    bool estaAprobado = false, esDesersion = true;
+    
+    //Operador // es el operador logico o
+    //Operador && es el operador logico y
+    
+    if(esDesersion){
+    	cout << "Usted ha aprobado la asignatura." << endl;
+	} else {
+		cout << "Usted no ha aprobado la asignatura." << endl;
+	}
+	return
+}
